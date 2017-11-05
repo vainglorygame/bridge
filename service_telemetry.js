0d229e85b310b5d1ba167e03f962dce2ff1aedbd
schneefux <schneefux+commit@schneefux.xyz>
2017-11-05 16:52:29 +0100

    do not redownload Telemetry

--- service_telemetry.js
+++ service_telemetry.js
@@ -23,15 +23,27 @@ module.exports = class Analyzer extends Service {
                 logger.info("requesting download for Telemetry", { api_id: req.params.match });
                 const category = req.params.category || "regular",
                     db = this.getDatabase(category),
-                    asset = await db.Asset.findOne({ where: {
-                        match_api_id: req.params.match
-                    } });
+                    asset = await db.Asset.findOne({
+                        attributes: [ "url" ],
+                        where: {
+                            match_api_id: req.params.match
+                        }
+                    });
+
                 if (asset == undefined) {
                     logger.error("could not find any assets for match",
                         { api_id: req.params.match });
                     res.sendStatus(404);
                     return;
                 }
+
+                const existing = await db.ParticipantPhases.count({ where: { match_api_id: req.params.match }});
+                if (existing > 0) {
+                    logger.info("Telemetry already downloaded", { api_id: req.params.match });
+                    res.sendStatus(304);
+                    return;
+                }
+
                 await this.forward(this.getTarget(category), asset.url, {
                     persistent: true,
                     headers: { match_api_id: req.params.match }
